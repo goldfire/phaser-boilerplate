03f03156598d1bf728466c9a70200869546104fb
Benji Kay <okaybenji@gmail.com>
2017-03-31 16:43:57 -0500

    update Player to extend Phaser.Sprite

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,8 +1,8 @@
 /**
  * Setup and control base player.
  */
-export default class Player {
-  constructor() {
-    
+export default class Player extends Phaser.Sprite {
+  constructor(config) {
+    super(config.game, config.x, config.y, config.key, config.frame);
   }
 }
